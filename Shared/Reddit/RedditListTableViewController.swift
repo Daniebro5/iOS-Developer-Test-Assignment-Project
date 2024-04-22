c6221071ab0ef485ee08f3ec93bab0f681fbe068
Danni Brito <danni.brito.-nd@disney.com>
2024-04-22 18:23:56 -0500

    Add Reddit data fetching to list view

--- Shared/Reddit/RedditListTableViewController.swift
+++ Shared/Reddit/RedditListTableViewController.swift
@@ -1,5 +1,59 @@
 import UIKit
 
-final class RedditListTableViewController: UITableViewController {
+final class RedditListTableViewController: UITableViewController, RedditViewModelDelegate {
+    private let viewModel: RedditViewModel
     
+    private let cellReuseIdentifier = "redditCell"
+    
+    init(viewModel: RedditViewModel = RedditViewModel()) {
+        self.viewModel = viewModel
+        super.init(style: .plain)
+        setupTableView()
+        viewModel.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44.0
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.fetchRedditPosts()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.redditPosts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        let redditPost = viewModel.redditPosts[indexPath.row]
+        cell.textLabel?.text = redditPost.title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let redditPost = viewModel.redditPosts[indexPath.row]
+        if let url = URL(string: redditPost.url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    func didFetchRedditData(success: Bool) {
+        if success {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Failed to fetch Reddit data.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
